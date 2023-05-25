08ff7d30b0571ba747d007f3168780a194d2b745
Maxsuel Aparecido Lima Santos <72948249+Maxsuel-Santos@users.noreply.github.com>
2023-05-25 20:32:50 -0300

    Addition of php 💥

---
+++ includes/avaliacoes.php
@@ -0,0 +1,29 @@
+<?php
+
+$avaliacoes = [
+    [
+        "nome" => "Clara Ribeiro avaliou:",
+        "avaliacao" => "Contratei um eletricista através do site e fiquei impressionado com a sua habilidade técnica. Fez um excelente trabalho!",
+        "estrelas" => '<img class="estrelas_clientes" src="../assets/img/Estrela.png" alt="Estrela"><img class="estrelas_clientes" src="../assets/img/Estrela.png" alt="Estrela"><img class="estrelas_clientes" src="../assets/img/Estrela.png" alt="Estrela"><img class="estrelas_clientes" src="../assets/img/Estrela.png" alt="Estrela"><img class="estrelas_clientes" src="../assets/img/Estrela.png" alt="Estrela">',
+        "funcionario" => "Para Marcos Paulo / Eletricista"
+    ],
+    [
+        "nome" => "João Silva avaliou:",
+        "avaliacao" => "O eletricista que encontrei no site foi rápido e eficaz. Resolveu o problema em minutos!",
+        "estrelas" => '<img class="estrelas_clientes_4" src="../assets/img/Estrela.png" alt="Estrela"><img class="estrelas_clientes_4" src="../assets/img/Estrela.png" alt="Estrela"><img class="estrelas_clientes_4" src="../assets/img/Estrela.png" alt="Estrela"><img class="estrelas_clientes_4" src="../assets/img/Estrela.png" alt="Estrela">',
+        "funcionario" => "Para Maria Joana / Eletricista"
+    ],
+    [
+        "nome" => "Mário Neto avaliou:",
+        "avaliacao" => "Serviço de eletricista de alta qualidade. Superou minhas expectativas!",
+        "estrelas" => '<img class="estrelas_clientes_4" src="../assets/img/Estrela.png" alt="Estrela"><img class="estrelas_clientes_4" src="../assets/img/Estrela.png" alt="Estrela"><img class="estrelas_clientes_4" src="../assets/img/Estrela.png" alt="Estrela"><img class="estrelas_clientes_4" src="../assets/img/Estrela.png" alt="Estrela">',
+        "funcionario" => "Para Carlos Alberto / Eletricista"
+    ],
+    [
+        "nome" => "Luísa Miranda avaliou:",
+        "avaliacao" => "Ótimo serviço! Indiquei para os meus amigos e quando precisar irei contratá-la novamente!",
+        "estrelas" => '<img class="estrelas_clientes" src="../assets/img/Estrela.png" alt="Estrela"><img class="estrelas_clientes" src="../assets/img/Estrela.png" alt="Estrela"><img class="estrelas_clientes" src="../assets/img/Estrela.png" alt="Estrela"><img class="estrelas_clientes" src="../assets/img/Estrela.png" alt="Estrela"><img class="estrelas_clientes" src="../assets/img/Estrela.png" alt="Estrela">',
+        "funcionario" => "Para Sandra Vieira / Eletricista"
+    ]
+
+];
